eb43dda9a53611677830d2063085c3b08ef0ae34
soimmary <55881643+soimmary@users.noreply.github.com>
2020-01-15 14:52:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,26 +7,27 @@ TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['inizio'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'cтартовое сообщение')
+    bot.send_message(message.chat.id, 'Ciao! C'è un bot che ti aiuta imparare le parole italiane!')
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['aiuto'])
 def help_message(message):
-    bot.send_message(message.chat.id, '\ciao или ciao – спрошу слово')
+    bot.send_message(message.chat.id, '\ciao o ciao – chieda una parola.\n
+    \parolanuova o parolanuova – ricorda una parola nuova.')
 
 
 @bot.message_handler(commands=['ciao'])
 def ciao_message(message):
-    if str(message.text).strip().lower() not in ['sonostanco', 'sono stanco', 'яустал', 'я устал']:
+    if str(message.text).strip().lower() not in ['sonostanca', 'sono stanca', 'яустала', 'я устала']:
         word, number = italian.askItalianWord()
         bot.send_message(message.chat.id, word)
         bot.register_next_step_handler(message=message, callback=ciao_message_main, number=number)
 
 
 def ciao_message_main(message, number: int):
-    if str(message.text).strip().lower() not in ['sonostanco', 'sono stanco', 'яустал', 'я устал']:
+    if str(message.text).strip().lower() not in ['sonostanca', 'sono stanca', 'яустала', 'я устала']:
         usersAnswer = str(message.text).lower()
         myDecision = italian.answerItalianWord(usersAnswer, number)
         bot.send_message(message.chat.id, myDecision)
@@ -35,7 +36,7 @@ def ciao_message_main(message, number: int):
         bot.register_next_step_handler(message=message, callback=ciao_message_main, number=number)
 
 
-@bot.message_handler(commands=['paroleNuove'])
+@bot.message_handler(commands=['parolanuova'])
 def parole_nuove(message):
     instructions = 'Inserisci la parola italiana e la parola russa.\nPer esempio: cioccolato - шоколад.'
     bot.send_message(message.chat.id, instructions)
@@ -58,7 +59,7 @@ def ciao_text_message(message):
     if str(message.text).lower() == 'ciao':
         ciao_message(message)
     else:
-        bot.send_message(message.chat.id, 'я не знаю таких команд /help')
+        bot.send_message(message.chat.id, 'non so questo ordine :( /aiuto')
 
 
 bot.polling(none_stop=True)
