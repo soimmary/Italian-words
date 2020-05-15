45c91a87b86b30a9477aeb3b11295bea415e7da6
soimmary <55881643+soimmary@users.noreply.github.com>
2020-05-15 11:04:54 +0300

    Update italian.py

--- italian.py
+++ italian.py
@@ -10,10 +10,15 @@ def create_basis(filename: str = 'italian.txt'):
     """ создает базу слов из ткстшника
         принимает название файла
     """
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            ital, rus = line.split('-')
-            WORDS_DICTIONARY[ital.strip().lower()] = rus.strip().lower()
+               
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open('Italian Words').sheet1
+    for row in range(1, sheet.row_count + 1):
+        WORDS_DICTIONARY[sheet.row_values(row)[0]] = sheet.row_values(row)[1]        
+    
 
 
 def choose_word():
