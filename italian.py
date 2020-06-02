2c988c268c5c774399e3807f6925d1ddd2a663e7
soimmary <55881643+soimmary@users.noreply.github.com>
2020-06-02 20:34:08 +0300

    Update italian.py

--- italian.py
+++ italian.py
@@ -1,65 +1,79 @@
 import random
 import matplotlib.pyplot as plt
-import collections 
+import collections
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 WORDS_DICTIONARY = {}  # *ital_word*: *rus_word*
-FORGOTTEN_WORDS = collections.Counter()
+USERS = {}
 
 
-def create_basis(tema):
-    """ создает базу слов в Google Sheets
+def create_basis():
+    """ создает базу слов из Google Sheets
     """
-    global WORDS_DICTIONARY         
+    global WORDS_DICTIONARY
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
-    
-# NEW ________
+
+    # NEW ________
     sheet = client.open('Italian Words')
     work_sheet = sheet.worksheet('parole')
     large_dictionary = {}
-    
-    for row in range(1, work_sheet.row_count + 1):
-        large_dictionary[work_sheet.row_values(row)[2]] = {}
-    for row in range(1, work_sheet.row_count + 1):
-        large_dictionary[work_sheet.row_values(row)[2]][work_sheet.row_values(row)[0]] \
-            = work_sheet.row_values(row)[1]
 
-    WORDS_DICTIONARY = large_dictionary[tema]
+    row_max = work_sheet.row_count + 1
+    for row in range(1, row_max):
+        theme = work_sheet.row_values(row)[2]
+        ital_word = work_sheet.row_values(row)[0]
+        rus_word = work_sheet.row_values(row)[1]
+        if theme not in large_dictionary:
+            large_dictionary[theme] = {}
+        large_dictionary[theme][ital_word] = rus_word
 
-# NEW ________    
+    WORDS_DICTIONARY = large_dictionary
 
 
-def choose_word():
+def add_new_user(user_id):
+    global USERS
+    user_info = {'language': 'ital🇮🇹 -> rus🇷🇺',
+                 'theme': 'il cibo 🍝',
+                 'word_pair': None,
+                 'forgotten_words': collections.Counter()}
+    USERS[user_id] = user_info
+
+
+def choose_word(user_id):
     """ спрашивает случайное слово из списка всех слов
-        принимает ответ пользователя answer
-        проверяет ответ пользователя
     """
-    word = random.choice(list(WORDS_DICTIONARY.items()))
-    return word
-
-
-def check_answer(answer, word, language):
-    right_answer = ''
+    theme = USERS[user_id]['theme']
+    language = USERS[user_id]['language']
+    USERS[user_id]['word_pair'] = random.choice(list(WORDS_DICTIONARY[theme].items()))
     if language == 'ital🇮🇹 -> rus🇷🇺':
-        right_answer = word[1]
+        return USERS[user_id]['word_pair'][0]
     elif language == 'rus🇷🇺 -> ital🇮🇹':
-        right_answer = word[0]
+        return USERS[user_id]['word_pair'][1]
+
+
+def check_answer(user_id, answer):
+    right_answer = ''
+    language = USERS[user_id]['language']
+    if language == 'ital🇮🇹 -> rus🇷🇺':
+        right_answer = USERS[user_id]['word_pair'][1]
+    elif language == 'rus🇷🇺 -> ital🇮🇹':
+        right_answer = USERS[user_id]['word_pair'][0]
     if right_answer == answer:
         approval = ('Giusto☺️!', 'Bene🤓!', 'Correttamente🤩!', 'Essato☺️!',
                     'Certo🥰!', 'Bravo👏🏻!', 'Bravissima🥳!')
         return random.choice(approval)
     else:
-        # new
-        FORGOTTEN_WORDS[word[0]] += 1
+        # какие итальянские слова часто забываешь
+        USERS[user_id][USERS[user_id]['word_pair'][0]] += 1
         return f'Ti sbagli ☹️:(\nLa risposta giusta: {right_answer}'
 
 
-def drawing_bar():
-    words = list(FORGOTTEN_WORDS.values())
-    number = list(FORGOTTEN_WORDS.keys())
+def drawing_bar(user_id):
+    words = list(USERS[user_id]['forgotten_words'].values())
+    number = list(USERS[user_id]['forgotten_words'].keys())
 
     plt.figure(figsize=(16, 9))
     plt.bar(number, words, color='pink')
@@ -67,6 +81,3 @@ def drawing_bar():
     plt.ylabel('Volte')
     plt.xlabel('Parole')
     plt.savefig(r'grafico.png', bbox_inches=0)
-
-
-#create_basis(tema)
