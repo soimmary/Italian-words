3fe2f765d9250034b0c116d86e9c8efb3e7d96a8
soimmary <55881643+soimmary@users.noreply.github.com>
2020-06-06 01:33:20 +0300

    Update italian.py

--- italian.py
+++ italian.py
@@ -4,7 +4,6 @@ import collections
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-
 WORDS_DICTIONARY = {}  # *ital_word*: *rus_word*
 USERS = {}
 
@@ -16,10 +15,12 @@ def create_basis():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
     client = gspread.authorize(creds)
+
     # NEW ________
     sheet = client.open('Italian Words')
     work_sheet = sheet.worksheet('parole')
     large_dictionary = {}
+
     row_max = work_sheet.row_count + 1
     for row in range(1, row_max):
         theme = work_sheet.row_values(row)[2]
@@ -28,9 +29,10 @@ def create_basis():
         if theme not in large_dictionary:
             large_dictionary[theme] = {}
         large_dictionary[theme][ital_word] = rus_word
+
     WORDS_DICTIONARY = large_dictionary
 
-    
+
 def add_new_user(user_id):
     global USERS
     user_info = {'language': 'ital🇮🇹 -> rus🇷🇺',
@@ -39,7 +41,7 @@ def add_new_user(user_id):
                  'forgotten_words': collections.Counter()}
     USERS[user_id] = user_info
 
-    
+
 def choose_word(user_id):
     """ спрашивает случайное слово из списка всех слов
     """
@@ -50,8 +52,8 @@ def choose_word(user_id):
         return USERS[user_id]['word_pair'][0]
     elif language == 'rus🇷🇺 -> ital🇮🇹':
         return USERS[user_id]['word_pair'][1]
-    
-    
+
+
 def check_answer(user_id, answer):
     right_answer = ''
     language = USERS[user_id]['language']
@@ -62,16 +64,17 @@ def check_answer(user_id, answer):
     if right_answer == answer:
         approval = ('Giusto☺️!', 'Bene🤓!', 'Correttamente🤩!', 'Essato☺️!',
                     'Certo🥰!', 'Bravo👏🏻!', 'Bravissima🥳!')
-         return random.choice(approval)
-     else:
-         # какие итальянские слова часто забываешь
-         USERS[user_id]['forgotten_words'][USERS[user_id]['word_pair'][0]] += 1
-         return f'Ti sbagli ☹️:(\nLa risposta giusta: {right_answer}'
+        return random.choice(approval)
+    else:
+        # какие итальянские слова часто забываешь
+        USERS[user_id]['forgotten_words'][USERS[user_id]['word_pair'][0]] += 1
+        return f'Ti sbagli ☹️:(\nLa risposta giusta: {right_answer}'
 
 
 def drawing_bar(user_id):
     words = list(USERS[user_id]['forgotten_words'].values())
     number = list(USERS[user_id]['forgotten_words'].keys())
+
     plt.figure(figsize=(16, 9))
     plt.bar(number, words, color='pink')
     plt.title('Le parole che dimentichi')
